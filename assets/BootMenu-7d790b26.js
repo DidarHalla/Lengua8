115a2b3f6f204179fc3b69fc73e3838e1e2d2b82
DidarHalla <didarhallayev255@gmail.com>
2024-01-16 19:23:47 +0500

    Updates

--- assets/BootMenu-7d790b26.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-edefa8ad.js";function s(){return e.jsxs("div",{className:"wrapper",children:[e.jsx("a",{href:"#about",children:"Главная"}),e.jsx("a",{href:"#about",children:"О приложении"}),e.jsx("a",{href:"#dowland",children:"Скачать приложение"}),e.jsx("a",{href:"#questions",children:"Вопросы"})]})}export{s as default};
