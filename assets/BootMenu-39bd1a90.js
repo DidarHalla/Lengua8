bd11c9e1fdc346ac9e0631720aa5d6a69ba87145
DidarHalla <didarhallayev255@gmail.com>
2024-01-16 20:35:53 +0500

    Updates

---
+++ assets/BootMenu-39bd1a90.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-1e597e3f.js";function s(){return e.jsxs("div",{className:"wrapper",children:[e.jsx("a",{href:"#about",children:"Главная"}),e.jsx("a",{href:"#about",children:"О приложении"}),e.jsx("a",{href:"#dowland",children:"Скачать приложение"}),e.jsx("a",{href:"#questions",children:"Вопросы"})]})}export{s as default};
--- assets/BootMenu-e40c49f5.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-58cd9454.js";function s(){return e.jsxs("div",{className:"wrapper",children:[e.jsx("a",{href:"#about",children:"Главная"}),e.jsx("a",{href:"#about",children:"О приложении"}),e.jsx("a",{href:"#dowland",children:"Скачать приложение"}),e.jsx("a",{href:"#questions",children:"Вопросы"})]})}export{s as default};
